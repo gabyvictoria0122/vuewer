ce7321ab147c68869ac4423ca401132d2c3f7eda
Gabriela Victoria <gabrielavictoria91@gmail.com>
2023-01-23 14:24:05 -0300

    debouncer

--- src/components/GithubRepo.vue
+++ src/components/GithubRepo.vue
@@ -27,16 +27,33 @@
 </template>
 
 <script>
+
+import {debouncerdecorator} from '@/helpers/debouncer.js'
   export default {
     data: () => ({
       user: null,
       usersearch: null,
       repolist: [],
       userlist: [],
+      userloading: false,
     }),
+    methods: {
+      procuraUsuariosGithub: debouncerdecorator(function () { // atenção: não use ()=>{} aqui. vai quebrar o decorator
+        console.log('faz de conta que eu perguntei pro github: ' + this.usersearch)
+        this.userloading = true
+        setTimeout(() => {
+          this.userlist = [
+            {login: 'joao'},
+            {login: 'jose'}
+          ]
+          this.userloading = false
+        }, 1000)
+      }, 500)
+    },
     watch: {
       usersearch () {
-          this.procuraUsuariosGithub()
+        this.userloading = true
+        this.procuraUsuariosGithub()
       },
     }
   }
