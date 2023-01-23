015ea1f5a42bb002dbf64ed498d629fd37b8a438
Gabriela Victoria <gabrielavictoria91@gmail.com>
2023-01-23 11:41:11 -0300

    agr vai começar

---
+++ src/components/GithubRepo.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-row class="text-center">
+      <v-col cols="6">
+        <v-autocomplete
+        v-model="user"
+        label="Encontre seu usuário"
+        :items="userlist"
+        :loading="userloading"
+        :search-input.sync="usersearch"
+        item-text="login"
+      />
+      </v-col>
+      <!-- <v-col cols="6">
+        <v-select
+          v-model="repo"
+          :items="repolist"
+          :loading="repoloading"
+          item-text="name"
+          label="Selecione o repositorio"
+          return-object
+          single-line
+        ></v-select>
+      </v-col> -->
+    </v-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      user: null,
+      usersearch: null,
+      repolist: [],
+      userlist: [],
+    }),
+    watch: {
+      usersearch () {
+          this.procuraUsuariosGithub()
+      },
+    }
+  }
+</script>
---
+++ src/components/GithubVuewer.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <GithubRepo/>
+  </v-container>
+</template>
+
+<script>
+  import GithubRepo from './GithubRepo'
+  export default {
+    components:{
+      GithubRepo,
+    },
+    data: () => ({
+    }),
+  }
+</script>
