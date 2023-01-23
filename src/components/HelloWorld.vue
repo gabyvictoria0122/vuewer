9a1889cf36f678c9e1a3c7f66c684378df73ee4d
Gabriela Victoria <gabrielavictoria91@gmail.com>
2023-01-23 08:41:06 -0300

    tem que ir poxa

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Gaby's Vuetify
+          Welcome to Gaby's Vuetify TEM QUE IR
         </h1>
 
         <p class="subheading font-weight-regular">
